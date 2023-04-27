ae9d2237c6b0e264c2f9306d9e015c9e1b202a2c
KeyWarden <ekeeley9@gmail.com>
2023-04-27 13:29:24 +0000

    add Tables table to database

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tables(models.Model):
+    size = models.IntegerField()
+    category = models.CharField()
+    upper_limit = models.IntegerField()
