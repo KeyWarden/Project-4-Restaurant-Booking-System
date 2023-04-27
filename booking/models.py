cc88e4db1681ab26715885ec820baae367a87fc9
KeyWarden <ekeeley9@gmail.com>
2023-04-27 13:31:23 +0000

    add Timeslot table to database

--- booking/models.py
+++ booking/models.py
@@ -3,7 +3,11 @@ from django.db import models
 # Create your models here.
 
 
-class Tables(models.Model):
+class Tables(models.Model()):
     size = models.IntegerField()
     category = models.CharField()
     upper_limit = models.IntegerField()
+
+
+class Timeslot(models.Model()):
+    time = models.TimeField()
