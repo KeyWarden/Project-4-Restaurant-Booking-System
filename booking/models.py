bb522f76d23147f8d82eda6fb909f97ab3bc4887
KeyWarden <ekeeley9@gmail.com>
2023-04-27 14:11:02 +0000

    add Booking table to database

--- booking/models.py
+++ booking/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
@@ -17,3 +19,15 @@ class Timeslot(models.Model):
 
     def __str__(self):
         return str(self.time)
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking")
+    date = models.DateField(null=False, blank=False)
+    number_of_people = models.IntegerField(null=False, blank=False)
+    table = models.ForeignKey(
+        Tables, on_delete=models.CASCADE, related_name="booking"
+    )
+    timeslot = models.ForeignKey(
+        Timeslot, on_delete=models.CASCADE, related_name="booking"
+    )
