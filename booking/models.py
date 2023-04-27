60c8a9d8e42cf4a84c12a11946fa13bc779dcd63
KeyWarden <ekeeley9@gmail.com>
2023-04-27 13:55:11 +0000

    register Tables and Timeslot to admin

--- booking/models.py
+++ booking/models.py
@@ -3,11 +3,17 @@ from django.db import models
 # Create your models here.
 
 
-class Tables(models.Model()):
-    size = models.IntegerField()
-    category = models.CharField()
-    upper_limit = models.IntegerField()
+class Tables(models.Model):
+    size = models.IntegerField(null=False, blank=False)
+    category = models.CharField(max_length=10, null=False, blank=False)
+    upper_limit = models.IntegerField(null=False, blank=False)
+
+    def __str__(self):
+        return self.category + str(self.pk)
 
 
-class Timeslot(models.Model()):
-    time = models.TimeField()
+class Timeslot(models.Model):
+    time = models.TimeField(null=False, blank=False)
+
+    def __str__(self):
+        return str(self.time)
