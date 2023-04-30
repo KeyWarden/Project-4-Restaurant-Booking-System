51a95f77a95fb4a6d31cb37fbc7afbf0da68b425
KeyWarden <ekeeley9@gmail.com>
2023-04-30 10:45:59 +0000

    add host

--- restaurant_booking_system/settings.py
+++ restaurant_booking_system/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-c)y4viujj3v&g9$4d7@-m)pu-v^r5pnpdj=#^6_*$i30)c@f&g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['project-4-restaurant-booking.herokuapp.com']
 
 
 # Application definition
